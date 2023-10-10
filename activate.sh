139e4f9bf00b9ddffec0b6ac1c30b5dcfed4cdab
Cavalo01 <124747668+Cavalo01@users.noreply.github.com>
2023-10-10 00:01:42 -0300

    activate.sh

--- activate.sh
+++ activate.sh
@@ -6,3 +6,4 @@ echo "chmod -R 777 *" >> ~/.bashrc
 echo "alias whoami='echo cavalinux'" >> ~/.bashrc
 echo "alias showfiles='ls'" >> ~/.bashrc
 echo "alias brutenmap='nmap -p-'" >> ~/.bashrc
+echo "alias setws='pkg install x11-repo && pkg install wireshark-qt" >> ~/.bashrc
