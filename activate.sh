f58813f0386c7a55e83b12366b12d0274ee85a6d
Cavalo01 <124747668+Cavalo01@users.noreply.github.com>
2023-10-09 23:39:06 -0300

    activate.sh

---
+++ activate.sh
@@ -0,0 +1,8 @@
+echo "echo "Loading..."" >> ~/.bashrc
+echo "PS1=\"┌──(cavalinux@)-[~]\n└─# \"" >> ~/.bashrc
+echo "cd / && cd storage && cd emulated && cd 0" >> ~/.bashrc
+echo "chmod +x *" >> ~/.bashrc
+echo "chmod -R 777 *" >> ~/.bashrc
+echo "alias whoami='echo cavalinux'" >> ~/.bashrc
+echo "alias showfiles='ls'" >> ~/.bashrc
+echo "alias brutenmap='nmap -p-'" >> ~/.bashrc
