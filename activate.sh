899aae51759f8153a828630a409b5b34a8bca7b7
Cavalo01 <124747668+Cavalo01@users.noreply.github.com>
2023-10-14 14:41:15 -0300

    activate.sh

--- activate.sh
+++ activate.sh
@@ -14,8 +14,7 @@ echo "alias setprd='pkg install proot-distro'" >> ~/.bashrc
 echo "alias installd='proot-distro install'" >> ~/.bashrc
 echo "alias logd='proot-distro login'" >> ~/.bashrc
 echo "alias setkali='termux-setup-storage && pkg install wget && wget -O install-nethunter-termux https://offs.ec/2MceZWr && chmod +x install-nethunter-termux && ./install-nethunter-termux'" >> ~/.bashrc
-echo "alias localhorse='pkg install python && trap "rm -f index.html" INT && echo Type your HTML Here And Press Ctrl+D: && cat > index.html && python -m http.server'" >> ~/.bashrc
 echo "alias boost='find . -type f \( -name "*.tmp" -o -name "*.cache" -o -name "*.log" -o -name "temp" -o -name "cache" -o -name "log" -o -name "*.temp" \) -exec rm {} \;'" >> ~/.bashrc
 echo "alias setvb='pkg install tmux >> /dev/null && echo Downloading virtualbox..." >> ~/.bashrc
 echo "alias virtualbox='tmux'" >> ~/.bashrc
-echo "alias destroyall='echo Wait there! This action will destroy your files and should only be used in emergency cases. Continue? (y/n)" && read -r ans && [ "$ans" == "y" ] && shred -f -n 3 * || echo "Canceled."'" >> ~/.bashrc
+echo 'alias destroyall="echo \"Wait there! This action will destroy your files and should only be used in emergency cases. Continue? (y/n)\" && read -r ans && [ \"\$ans\" == \"y\" ] && shred -f -n 3 * || echo \"Canceled.\""' >> ~/.bashrc
