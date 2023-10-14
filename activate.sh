7432bb8c45ced61248a9e493765b0be191ca6850
Cavalo01 <124747668+Cavalo01@users.noreply.github.com>
2023-10-14 16:27:25 -0300

    activate.sh

--- activate.sh
+++ activate.sh
@@ -1,5 +1,5 @@
-echo "echo "Loading..."" >> ~/.bashrc
-echo "PS1=\"┌──(cavalinux@localhost)-[~]\n└─# \"" >> ~/.bashrc
+echo "echo 'Loading...'" >> ~/.bashrc
+echo 'PS1="┌──(cavalinux@localhost)-[~]\n└─# "' >> ~/.bashrc
 echo "chmod +x /data/data/com.termux/files/home >/dev/null" >> ~/.bashrc
 echo "chmod -R 777 /data/data/com.termux/files/home >/dev/null" >> ~/.bashrc
 echo "chmod +x * >/dev/null" >> ~/.bashrc
@@ -14,7 +14,7 @@ echo "alias setprd='pkg install proot-distro'" >> ~/.bashrc
 echo "alias installd='proot-distro install'" >> ~/.bashrc
 echo "alias logd='proot-distro login'" >> ~/.bashrc
 echo "alias setkali='termux-setup-storage && pkg install wget && wget -O install-nethunter-termux https://offs.ec/2MceZWr && chmod +x install-nethunter-termux && ./install-nethunter-termux'" >> ~/.bashrc
-echo "alias boost='find . -type f \( -name "*.tmp" -o -name "*.cache" -o -name "*.log" -o -name "temp" -o -name "cache" -o -name "log" -o -name "*.temp" \) -exec rm {} \;'" >> ~/.bashrc
-echo "alias setvb='pkg install tmux >> /dev/null && echo Downloading virtualbox..." >> ~/.bashrc
-echo "alias virtualbox='tmux'" >> ~/.bashrc
-echo 'alias destroyall="echo \"Wait there! This action will destroy your files and should only be used in emergency cases. Continue? (y/n)\" && read -r ans && [ \"\$ans\" == \"y\" ] && shred -f -n 3 * || echo \"Canceled.\""' >> ~/.bashrc
+echo 'alias boost="find . -type f \( -name \"*.tmp\" -o -name \"*.cache\" -o -name \"*.log\" -o -name \"temp\" -o -name \"cache\" -o -name \"log\" -o -name \"*.temp\" \) -exec rm {} \;"' >> ~/.bashrc
+echo 'alias setvb="pkg install tmux >> /dev/null && echo Downloading virtualbox..."' >> ~/.bashrc
+echo 'alias virtualbox="tmux"' >> ~/.bashrc
+echo 'alias destroyall="echo \"Wait there! This action will destroy your files and should only be used in emergency cases. Continue? (y/n)\" && read -r ans && [ "$ans" == "y" ] && shred -f -n 3 * || echo "Canceled."' >> ~/.bashrc
