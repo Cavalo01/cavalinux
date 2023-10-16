04d6ea478db61fe02e9dc1cdcb27d920f51f0310
Cavalo01 <124747668+Cavalo01@users.noreply.github.com>
2023-10-16 17:42:00 -0300

    activate.sh

--- activate.sh
+++ activate.sh
@@ -20,3 +20,5 @@ echo 'alias virtualbox="tmux"' >> ~/.bashrc
 echo "alias destroyall='echo \"Wait there! This action will destroy your files and should only be used in emergency cases. Continue? (y/n)\" && read -r ans && [ \"\$ans\" == \"y\" ] && shred -f -n 3 * || echo \"Canceled.\"'" >> ~/.bashrc
 echo "alias setnslp='pkg install dnsutils'" >> ~/.bashrc
 echo "alias setwordlist='wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt > /dev/null 2>&1 && mv 10-million-password-list-top-1000000.txt wordlist.txt && echo Wordlist downloaded! Size: 8 MB, Name: wordlist.txt'" >> ~/.bashrc
+echo "alias setmsf='pkg install git ruby && git clone https://github.com/rapid7/metasploit-framework.git && cd metasploit-framework && gem install bundler && bundle install && chmod +x msfconsole && mv * /storage/emulated/0'" >> ~/.bashrc
+echo "alias msfconsole='./msfconsole'" >> ~/.bashrc
