037a8c60e42a14b87fa0b9fb3649d31f2ef86863
Cavalo01 <124747668+Cavalo01@users.noreply.github.com>
2023-10-20 23:22:42 -0300

    activate.sh

--- activate.sh
+++ activate.sh
@@ -1,9 +1,9 @@
 echo "echo 'Loading...'" >> ~/.bashrc
 echo 'PS1="\[\033[1;31m\]┌──(cavalinux@localhost)-[~]\n\[\033[0;31m\]└─# \[\033[0m\]"' >> ~/.bashrc
-echo "chmod +x ~/termux_setup.sh > /dev/null 2>&1" >> ~/.bashrc
-echo "chmod -R 777 ~/termux_setup.sh > /dev/null 2>&1" >> ~/.bashrc
-echo "chmod +x ~/scripts/* > /dev/null 2>&1" >> ~/.bashrc
-echo "chmod -R 777 ~/scripts/* > /dev/null 2>&1" >> ~/.bashrc
+echo "chmod +x /data/data/com.termux/files/home > /dev/null 2>&1" >> ~/.bashrc
+echo "chmod -R 777 /data/data/com.termux/files/home > /dev/null 2>&1" >> ~/.bashrc
+echo "chmod +x * > /dev/null 2>&1" >> ~/.bashrc
+echo "chmod -R 777 * > /dev/null 2>&1" >> ~/.bashrc
 echo "cd / && cd storage && cd emulated && cd 0 || cd " >> ~/.bashrc
 echo "alias whoami='echo cavalinux'" >> ~/.bashrc
 echo "alias showfiles='ls'" >> ~/.bashrc
@@ -20,11 +20,11 @@ echo 'alias virtualbox="tmux"' >> ~/.bashrc
 echo "alias destroyall='echo \"Wait there! This action will destroy your files and should only be used in emergency cases. Continue? (y/n)\" && read -r ans && [ \"\$ans\" == \"y\" ] && shred -f -n 3 * || echo \"Canceled.\"'" >> ~/.bashrc
 echo "alias setnslp='pkg install dnsutils'" >> ~/.bashrc
 echo "alias setwordlist='wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt > /dev/null 2>&1 && mv 10-million-password-list-top-1000000.txt wordlist.txt && echo Wordlist downloaded! Size: 8 MB, Name: wordlist.txt'" >> ~/.bashrc
-echo "alias setmsf='pkg install git ruby && git clone https://github.com/rapid7/metasploit-framework.git && cd metasploit-framework && gem install bundler && bundle install && chmod +x msfconsole && mv * ~/metasploit'" >> ~/.bashrc
+echo "alias setmsf='pkg install git ruby && git clone https://github.com/rapid7/metasploit-framework.git && cd metasploit-framework && gem install bundler && bundle install && chmod +x msfconsole && mv * /storage/emulated/0'" >> ~/.bashrc
 echo "alias msfconsole='./msfconsole'" >> ~/.bashrc
 echo '
 bruteftp() {
-  echo "Welcome to BruteFTP!"
+echo "Welcome to BruteFTP!"
   read -p "Insert your target (without '\''ftp://, http:// or https://\''): " target_site
 
   common_ftp_ports=("21" "22" "80" "443")  
@@ -55,28 +55,58 @@ bruteftp() {
     "sharedrive"
     "quickconnect"
     "datahubpro"
-  read -p "What distro you want to use?" distro
+    "transferdata"
+    "filebox"
+    "speedyshare"
+    "datahub"
+    "cloudftpx"
+    "rapidconnect"
+    "globalfile"
+    "powerupload"
+    "superfile"
+    "dataelite"
+    "proftpx"
+    "fastshare"
+    "dataxpress"
+    "uptoload"
+    "maxfile"
+    "datasend"
+    "datasyncpro"
+    "syncvault"
+    "filestore"
+    "dataxpresspro"
+    "swiftfileshare"
+    "turboconnect"
+    "rapidstorage"
+    "prodatahub"
+    "uptodata"
+    "maxtransfer"
+    "datahubplus"
+    "cloudftpxpro"
+    "dataxpressshare"
+    "sharedata"
+    "datasyncplus"
+    "rapidsharepro"
+    "swiftupload"
+  )
 
-  if proot-distro login "$distro"; then
-    echo "Success!"
-  else
-    if [[ $? -eq 3 ]]; then
-      echo 
-    else
-      proot-distro install $distro && proot-distro login $distro || echo "Sorry, We Dont Have This Distro."
-    fi
-  fi
-} ' >> ~/.bashrc
+  for server in "${common_ftp_servers[@]}"; do
+for port in "${common_ftp_ports[@]}"; do
+      ftp_server="${server}.${target_site}"
+      if nc -z -w 5 "$ftp_server" "$port"; then
+        echo "FTP Server Found on port $port! $ftp_server"
+        return
+      fi
+    done
+  done
+
+  echo "No FTP Server Found."
+}
+' >> ~/.bashrc
 echo '
 servelinch(){ 
-  echo " ____                      _ _            _     "
-  echo "/ ___|  ___ _ ____   _____| (_)_ __   ___| |__  "
-  echo "\___ \ / _ \ '__\ \ / / _ \ | | '_ \ / __| '_\ "
-  echo " ___) |  __/ |   \ V /  __/ | | | | | (__| | | |"
-  echo "|____/ \___|_|    \_/ \___|_|_|_| |_|\___|_| |_|"
-  echo
 
-  read -p "Digite seu alvo: " site
+  read -p "Enter your target: " site
 
   response=$(curl -I -s "$site" | grep "Server")
 
